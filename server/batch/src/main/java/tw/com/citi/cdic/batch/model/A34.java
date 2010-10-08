059a741da51b5cd8ad12c7d611912741f3fc52b2
LancelotLiu <lancelot.yl.liu@gmail.com>
2010-10-08 16:35:29 +0800

    add A34 Layout

---
+++ server/batch/src/main/java/tw/com/citi/cdic/batch/model/A34.java
@@ -0,0 +1,110 @@
+package tw.com.citi.cdic.batch.model;
+
+/**
+ * A34, CDICF10 Layout
+ * 
+ * @author Lancelot
+ * @since 2010/10/8
+ */
+public class A34 {
+    String unit;
+    String branchNo;
+    String srNo;
+    String apNo;
+    String applyDate;
+    String applyKind;
+    String billNo;
+    String currencyCode;
+    double billAmt;
+    String billDate;
+    String accName;
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getBranchNo() {
+        return branchNo;
+    }
+
+    public void setBranchNo(String branchNo) {
+        this.branchNo = branchNo;
+    }
+
+    public String getSrNo() {
+        return srNo;
+    }
+
+    public void setSrNo(String srNo) {
+        this.srNo = srNo;
+    }
+
+    public String getApNo() {
+        return apNo;
+    }
+
+    public void setApNo(String apNo) {
+        this.apNo = apNo;
+    }
+
+    public String getApplyDate() {
+        return applyDate;
+    }
+
+    public void setApplyDate(String applyDate) {
+        this.applyDate = applyDate;
+    }
+
+    public String getApplyKind() {
+        return applyKind;
+    }
+
+    public void setApplyKind(String applyKind) {
+        this.applyKind = applyKind;
+    }
+
+    public String getBillNo() {
+        return billNo;
+    }
+
+    public void setBillNo(String billNo) {
+        this.billNo = billNo;
+    }
+
+    public String getCurrencyCode() {
+        return currencyCode;
+    }
+
+    public void setCurrencyCode(String currencyCode) {
+        this.currencyCode = currencyCode;
+    }
+
+    public double getBillAmt() {
+        return billAmt;
+    }
+
+    public void setBillAmt(double billAmt) {
+        this.billAmt = billAmt;
+    }
+
+    public String getBillDate() {
+        return billDate;
+    }
+
+    public void setBillDate(String billDate) {
+        this.billDate = billDate;
+    }
+
+    public String getAccName() {
+        return accName;
+    }
+
+    public void setAccName(String accName) {
+        this.accName = accName;
+    }
+
+}
