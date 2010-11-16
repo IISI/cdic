ce710ead1477a85135fb7337a752d44b3b2739ff
LancelotLiu <lancelot.yl.liu@gmail.com>
2010-11-16 13:43:26 +0800

    rename CDICT27H to CDICT27

--- server/batch/src/main/java/tw/com/citi/cdic/batch/item/SBT27Processor.java
+++ server/batch/src/main/java/tw/com/citi/cdic/batch/item/SBT27Processor.java
@@ -2,17 +2,17 @@ package tw.com.citi.cdic.batch.item;
 
 import org.springframework.batch.item.ItemProcessor;
 
-import tw.com.citi.cdic.batch.model.CDICT27H;
+import tw.com.citi.cdic.batch.model.CDICT27;
 import tw.com.citi.cdic.batch.model.T01;
 
 /**
  * @author Lancelot
  * @since 2010/10/11
  */
-public class SBT27Processor implements ItemProcessor<CDICT27H, T01> {
+public class SBT27Processor implements ItemProcessor<CDICT27, T01> {
 
     @Override
-    public T01 process(CDICT27H item) throws Exception {
+    public T01 process(CDICT27 item) throws Exception {
         T01 t27 = null;
         if (item != null && "IM".equals(item.getSourceIndicator())) {
             t27 = new T01();
