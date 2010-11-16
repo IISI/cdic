6d84f155e1eca62965360af431c71fffb32576af
Chih-Liang Chang <itscchang@gmail.com>
2010-11-16 10:47:31 +0800

    把 SUC002 的中文錯誤訊息移出來到 messages.properties 檔。

--- client/tw.com.citi.cdic.client/src/tw/com/citi/cdic/utils/Messages.java
+++ client/tw.com.citi.cdic.client/src/tw/com/citi/cdic/utils/Messages.java
@@ -11,6 +11,7 @@ public class Messages extends NLS {
     public static String STATUS_4;
     public static String STATUS_5;
     public static String STATUS_6;
+    public static String SUC002Handler_InitStateError;
     static {
         // initialize resource bundle
         NLS.initializeMessages(BUNDLE_NAME, Messages.class);
