103071b80492f62e2caa90236ee16789068a1f54
Chih-Liang Chang <itscchang@gmail.com>
2010-11-19 11:14:34 +0800

    更新 SBF05 LUS reader 的 sql 指令，並新增 processor 處理資料的轉換。

---
+++ server/batch/src/main/java/tw/com/citi/cdic/batch/item/SBF05Processor.java
@@ -0,0 +1,28 @@
+package tw.com.citi.cdic.batch.item;
+
+import org.springframework.batch.item.ItemProcessor;
+
+import tw.com.citi.cdic.batch.model.A24;
+import tw.com.citi.cdic.batch.model.Lus;
+
+/**
+ * @author Chih-Liang Chang
+ * @since 2010/11/19
+ */
+public class SBF05Processor implements ItemProcessor<Lus, A24> {
+
+    @Override
+    public A24 process(Lus item) throws Exception {
+        A24 a24 = new A24();
+        a24.setSrNo(item.getAcctNo());
+        a24.setCharCode(item.getProdName());
+        a24.setCustomerId(item.getUnino());
+        a24.setCustomerName(item.getName());
+        a24.setCustomerBusinessCode(item.getNewBCode());
+        a24.setBalance(item.getBalance());
+        a24.setIntPayable(item.getInterest());
+        a24.setAddress(item.getCommAdr());
+        return a24;
+    }
+
+}
