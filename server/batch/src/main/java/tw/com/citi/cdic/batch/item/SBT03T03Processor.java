aceaf618782cbe1df0f8ccdfffc8bb78b2887f17
LancelotLiu <lancelot.yl.liu@gmail.com>
2010-11-23 16:12:49 +0800

    修正 T03, T12 無資料的問題。

--- server/batch/src/main/java/tw/com/citi/cdic/batch/item/SBT03T03Processor.java
+++ server/batch/src/main/java/tw/com/citi/cdic/batch/item/SBT03T03Processor.java
@@ -17,7 +17,7 @@ public class SBT03T03Processor implements ItemProcessor<CDICT03G, T01> {
     @Override
     public T01 process(CDICT03G item) throws Exception {
         T01 t03 = null;
-        if (item != null && ("IM ".equals(item.getSystemId()) || "MD ".equals(item.getSystemId()))) {
+        if (item != null && ("IM".equals(item.getSystemId().trim()) || "MD".equals(item.getSystemId().trim()))) {
             t03 = new T01();
             t03.setCode(item.getProductCode());
             t03.setDescription(item.getDescription());
