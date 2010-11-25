e24634b297281cf3bce51fd949d810876eb4bc98
Chih-Liang Chang <itscchang@gmail.com>
2010-11-25 16:18:54 +0800

    完成報表交易。

--- server/batch/src/main/java/tw/com/citi/cdic/batch/step/tasklet/GenerateReportTasklet.java
+++ server/batch/src/main/java/tw/com/citi/cdic/batch/step/tasklet/GenerateReportTasklet.java
@@ -3,6 +3,7 @@ package tw.com.citi.cdic.batch.step.tasklet;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import net.sf.jasperreports.engine.JRDataSource;
@@ -13,8 +14,14 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.ui.jasperreports.JasperReportsUtils;
+
+import tw.com.citi.cdic.batch.dao.ReportDao;
+import tw.com.citi.cdic.batch.dao.TableFlowDao;
+import tw.com.citi.cdic.batch.model.Report;
+import tw.com.citi.cdic.batch.model.TableFlow;
 
 /**
  * @author Chih-Liang Chang
@@ -24,12 +31,23 @@ public class GenerateReportTasklet implements Tasklet {
 
     private Resource resource;
 
+    private TableFlowDao tableFlowDao;
+
+    private ReportDao reportDao;
+
     @Override
     public RepeatStatus execute(StepContribution contribution,
             ChunkContext chunkContext) throws Exception {
-        JasperReport report = JasperCompileManager.compileReport("report.jrxml");
-        Map parameters = new HashMap();
-        JRDataSource reportData = JasperReportsUtils.convertReportData(null);
+        Resource reportTemplate = new ClassPathResource("report.jrxml");
+        JasperReport report = JasperCompileManager.compileReport(reportTemplate.getInputStream());
+        
+        Map<String, Object> parameters = new HashMap<String, Object>();
+        TableFlow tableFlow = tableFlowDao.getTableFlow();
+        parameters.put("baseDate", tableFlow.getCustDate());
+        
+        List<Report> data = reportDao.findAll();
+        JRDataSource reportData = JasperReportsUtils.convertReportData(data);
+        
         File file = resource.getFile();
         FileOutputStream stream = new FileOutputStream(file, false);
         JasperReportsUtils.renderAsPdf(report, parameters, reportData, stream);
@@ -38,6 +56,14 @@ public class GenerateReportTasklet implements Tasklet {
 
     public void setResource(Resource resource) {
         this.resource = resource;
+    }
+
+    public void setTableFlowDao(TableFlowDao tableFlowDao) {
+        this.tableFlowDao = tableFlowDao;
+    }
+
+    public void setReportDao(ReportDao reportDao) {
+        this.reportDao = reportDao;
     }
 
 }
