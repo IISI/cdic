e307e33b55c267140d9c9c216dd80e3a54559ea3
Chih-Liang Chang <itscchang@gmail.com>
2010-11-26 15:47:48 +0800

    launch.cmd 啟動及結束時，需記錄 log。

--- server/batch/launch.cmd
+++ server/batch/launch.cmd
@@ -1,4 +1,6 @@
 @echo off
+echo Batch started on %date% %time% - %* >> "%~dp0log\psexec.log"
 net use z: \\london\cdic_share replace_with_your_password /user:replace_with_your_username
 "%~dp0jre6\bin\java.exe" -cp "%~dp0conf\;%~dp0lib\*" -DLOG_HOME="%~dp0log" -DFOLDER.PROCESS="Z:/process" -DFOLDER.PROCESS.OUT="Z:/process/out" org.springframework.batch.core.launch.support.CommandLineJobRunner batch.xml %*
 net use z: /delete
+echo Batch ended on %date% %time% - %* >> "%~dp0log\psexec.log"
