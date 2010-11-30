2b3e605d95b114e0d02423dc4ce7c1a64dae0afc
Chih-Liang Chang <itscchang@gmail.com>
2010-11-30 11:57:02 +0800

    修正 SBF18 Bus 無法 casting 成 A11 的錯誤。

--- server/batch/src/main/java/tw/com/citi/cdic/batch/item/SBF18Processor.java
+++ server/batch/src/main/java/tw/com/citi/cdic/batch/item/SBF18Processor.java
@@ -12,12 +12,12 @@ import tw.com.citi.cdic.batch.dao.A22Dao;
 import tw.com.citi.cdic.batch.dao.A23Dao;
 import tw.com.citi.cdic.batch.dao.A24Dao;
 import tw.com.citi.cdic.batch.dao.CDICF20Dao;
-import tw.com.citi.cdic.batch.model.A11;
 import tw.com.citi.cdic.batch.model.A21;
 import tw.com.citi.cdic.batch.model.A22;
 import tw.com.citi.cdic.batch.model.A23;
 import tw.com.citi.cdic.batch.model.A24;
 import tw.com.citi.cdic.batch.model.A61;
+import tw.com.citi.cdic.batch.model.Bus;
 import tw.com.citi.cdic.batch.model.CDICF20;
 import tw.com.citi.cdic.batch.model.SBF18Output;
 import tw.com.citi.cdic.batch.utils.MaskUtils;
@@ -26,7 +26,7 @@ import tw.com.citi.cdic.batch.utils.MaskUtils;
  * @author Chih-Liang Chang
  * @since 2010/10/14
  */
-public class SBF18Processor implements ItemProcessor<A11, SBF18Output> {
+public class SBF18Processor implements ItemProcessor<Bus, SBF18Output> {
 
     private CDICF20Dao CDICF20Dao;
 
@@ -43,15 +43,15 @@ public class SBF18Processor implements ItemProcessor<A11, SBF18Output> {
     private int writeSampleFrequency = 1000;
 
     @Override
-    public SBF18Output process(A11 item) throws Exception {
+    public SBF18Output process(Bus item) throws Exception {
         double temp7 = 0, temp8 = 0, temp11 = 0, temp12 = 0, temp9 = 0, temp10 = 0, temp13 = 0, temp14 = 0, temp17 = 0, temp18 = 0, temp15 = 0, temp16 = 0, temp19 = 0, temp20 = 0;
         SBF18Output out = new SBF18Output();
         
         // 1. Summary 台幣要保金額
-        List<A21> a21s = a21Dao.findByCustomerIdAndJointCode(item.getId(), "0", "A21");
-        List<A22> a22s = a22Dao.findByCustomerIdAndJointCode(item.getId(), "0", "A22");
-        List<A23> a23s = a23Dao.findByCustomerIdAndJointCode(item.getId(), "0");
-        List<A24> a24s = a24Dao.findByCustomerId(item.getId());
+        List<A21> a21s = a21Dao.findByCustomerIdAndJointCode(item.getCustNumb(), "0", "A21");
+        List<A22> a22s = a22Dao.findByCustomerIdAndJointCode(item.getCustNumb(), "0", "A22");
+        List<A23> a23s = a23Dao.findByCustomerIdAndJointCode(item.getCustNumb(), "0");
+        List<A24> a24s = a24Dao.findByCustomerId(item.getCustNumb());
         for (A21 a21 : a21s) {
             temp7 += a21.getAccountBalance();
             temp8 += a21.getIntPayable();
@@ -77,9 +77,9 @@ public class SBF18Processor implements ItemProcessor<A11, SBF18Output> {
         out.getA24List().addAll(a24s);
         
         // 2. Summary 台幣聯名戶金額
-        a21s = a21Dao.findByCustomerIdAndJointCode(item.getId(), "1", "A21");
-        a22s = a22Dao.findByCustomerIdAndJointCode(item.getId(), "1", "A22");
-        a23s = a23Dao.findByCustomerIdAndJointCode(item.getId(), "1");
+        a21s = a21Dao.findByCustomerIdAndJointCode(item.getCustNumb(), "1", "A21");
+        a22s = a22Dao.findByCustomerIdAndJointCode(item.getCustNumb(), "1", "A22");
+        a23s = a23Dao.findByCustomerIdAndJointCode(item.getCustNumb(), "1");
         for (A21 a21 : a21s) {
             temp11 += a21.getAccountBalance();
             temp12 += a21.getIntPayable();
@@ -96,7 +96,7 @@ public class SBF18Processor implements ItemProcessor<A11, SBF18Output> {
         out.getA23List().addAll(a23s);
         
         // 3. Summary 台幣非要保金額
-        a22s = a22Dao.findByCustomerIdAndJointCodeAndCharCodeAndAmount(item.getId(), "A22");
+        a22s = a22Dao.findByCustomerIdAndJointCodeAndCharCodeAndAmount(item.getCustNumb(), "A22");
         for (A22 a22 : a22s) {
             temp9 += a22.getAmount();
             temp10 += a22.getIntPayable();
@@ -104,8 +104,8 @@ public class SBF18Processor implements ItemProcessor<A11, SBF18Output> {
         out.getA22List().addAll(a22s);
         
         // 4. Summary 外幣要保金額
-        a21s = a21Dao.findByCustomerIdAndJointCode(item.getId(), "0", "B21");
-        a22s = a22Dao.findByCustomerIdAndJointCode(item.getId(), "0", "B22");
+        a21s = a21Dao.findByCustomerIdAndJointCode(item.getCustNumb(), "0", "B21");
+        a22s = a22Dao.findByCustomerIdAndJointCode(item.getCustNumb(), "0", "B22");
         for (A21 b21 : a21s) {
             CDICF20 cdicF20 = CDICF20Dao.findByCurrencyCode(b21.getCurrencyCode());
             temp13 += b21.getAccountBalance() * cdicF20.getTransRate();
@@ -120,8 +120,8 @@ public class SBF18Processor implements ItemProcessor<A11, SBF18Output> {
         out.getB22List().addAll(a22s);
         
         // 5. Summary 外幣聯名戶金額
-        a21s = a21Dao.findByCustomerIdAndJointCode(item.getId(), "1", "B21");
-        a22s = a22Dao.findByCustomerIdAndJointCode(item.getId(), "1", "B22");
+        a21s = a21Dao.findByCustomerIdAndJointCode(item.getCustNumb(), "1", "B21");
+        a22s = a22Dao.findByCustomerIdAndJointCode(item.getCustNumb(), "1", "B22");
         for (A21 b21 : a21s) {
             CDICF20 cdicF20 = CDICF20Dao.findByCurrencyCode(b21.getCurrencyCode());
             temp17 += b21.getAccountBalance() * cdicF20.getTransRate();
@@ -136,7 +136,7 @@ public class SBF18Processor implements ItemProcessor<A11, SBF18Output> {
         out.getB22List().addAll(a22s);
         
         // 6. Summary 外幣非要保金額
-        a22s = a22Dao.findByCustomerIdAndJointCodeAndCharCodeOrSdCaseAndAmount(item.getId(), "B22");
+        a22s = a22Dao.findByCustomerIdAndJointCodeAndCharCodeOrSdCaseAndAmount(item.getCustNumb(), "B22");
         for (A22 b22 : a22s) {
             CDICF20 cdicF20 = CDICF20Dao.findByCurrencyCode(b22.getCurrencyCode());
             temp15 += b22.getAmount() * cdicF20.getTransRate();
@@ -145,8 +145,8 @@ public class SBF18Processor implements ItemProcessor<A11, SBF18Output> {
         out.getB22List().addAll(a22s);
         
         // 7. Summary OBU要保金額
-        a21s = a21Dao.findByCustomerId(item.getId(), "C21");
-        a22s = a22Dao.findByCustomerId(item.getId(), "C22");
+        a21s = a21Dao.findByCustomerId(item.getCustNumb(), "C21");
+        a22s = a22Dao.findByCustomerId(item.getCustNumb(), "C22");
         for (A21 c21 : a21s) {
             CDICF20 cdicF20 = CDICF20Dao.findByCurrencyCode(c21.getCurrencyCode());
             temp19 += c21.getAccountBalance() * cdicF20.getTransRate();
@@ -162,7 +162,7 @@ public class SBF18Processor implements ItemProcessor<A11, SBF18Output> {
         A61 a61 = new A61();
         a61.setUnit("021");
         a61.setBranchNo("0000");
-        a61.setCustId(item.getHeadId());
+        a61.setCustId(item.getNatnidRegnnumb());
         a61.setDate(null);
         a61.setAcctBalance(temp7);
         a61.setAcctInt(temp8);
