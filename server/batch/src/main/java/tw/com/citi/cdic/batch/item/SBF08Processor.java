ba7e714523d8467c6224b73ceecd0567f1175ebc
Chih-Liang Chang <itscchang@gmail.com>
2011-02-21 17:06:12 +0800

    調整 F08，原先是以 joint account 檔中的 GRB 及 account 欄位為 key 去搜尋資料。現在改成只用 GRB 欄位為 key 去搜尋資料。

--- server/batch/src/main/java/tw/com/citi/cdic/batch/item/SBF08Processor.java
+++ server/batch/src/main/java/tw/com/citi/cdic/batch/item/SBF08Processor.java
@@ -1,5 +1,8 @@
 package tw.com.citi.cdic.batch.item;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.batch.item.ItemProcessor;
 
 import tw.com.citi.cdic.batch.dao.A21Dao;
@@ -15,7 +18,7 @@ import tw.com.citi.cdic.batch.model.JointAcclist;
  * @author Chih-Liang Chang
  * @since 2010/10/13
  */
-public class SBF08Processor implements ItemProcessor<JointAcclist, A31> {
+public class SBF08Processor implements ItemProcessor<JointAcclist, List<A31>> {
 
     private A21Dao a21Dao;
 
@@ -30,118 +33,135 @@ public class SBF08Processor implements ItemProcessor<JointAcclist, A31> {
     private int writeSampleFrequency = 1000;
 
     @Override
-    public A31 process(JointAcclist item) throws Exception {
+    public List<A31> process(JointAcclist item) throws Exception {
         // 如果 account 的第一碼是'_'的話，就把它拿掉
         String account = item.getAccount();
         if (account.startsWith("_")) {
             account = account.replaceFirst("_", "");
         }
         
-        A21 a21 = a21Dao.findByCustomerIdAndSrNo(item.getGRB(), account, "A21");
-        if (a21 != null) {
+        List<A31> results = new ArrayList<A31>();
+        
+        List<A21> a21s = a21Dao.findByCustomerId(item.getGRB(), "A21");
+        if (a21s != null && !a21s.isEmpty()) {
             if (type == 1) {
-                A31 a31 = new A31();
-                a31.setUnit("021");
-                a31.setSrNo(account);
-                a31.setLocateRate(100.00);
-                a31.setBranchNo(a21.getBranchNo());
-                a31.setCurrencyCode(a21.getCurrencyCode());
-                a31.setCustomerId(a21.getCustomerId());
-                
-                processCount++;
-                if (processCount % writeSampleFrequency == 1) {
-                    a31.setSample(true);
+                for (A21 a21 : a21s) {
+                    A31 a31 = new A31();
+                    a31.setUnit("021");
+                    a31.setSrNo(account);
+                    a31.setLocateRate(100.00);
+                    a31.setBranchNo(a21.getBranchNo());
+                    a31.setCurrencyCode(a21.getCurrencyCode());
+                    a31.setCustomerId(a21.getCustomerId());
+                    
+                    processCount++;
+                    if (processCount % writeSampleFrequency == 1) {
+                        a31.setSample(true);
+                    }
+                    
+                    results.add(a31);
                 }
-                
-                return a31;
+                return results;
             } else {
                 return null;
             }
         }
         
-        A21 b21 = a21Dao.findByCustomerIdAndSrNo(item.getGRB(), account, "B21");
-        if (b21 != null) {
+        List<A21> b21s = a21Dao.findByCustomerId(item.getGRB(), "B21");
+        if (b21s != null && !b21s.isEmpty()) {
             if (type == 2) {
-                A31 b31 = new A31();
-                b31.setUnit("021");
-                b31.setSrNo(account);
-                b31.setLocateRate(100.00);
-                b31.setBranchNo(b21.getBranchNo());
-                b31.setCurrencyCode(b21.getCurrencyCode());
-                b31.setCustomerId(b21.getCustomerId());
-                
-                processCount++;
-                if (processCount % writeSampleFrequency == 1) {
-                    b31.setSample(true);
+                for (A21 b21 : b21s) {
+                    A31 b31 = new A31();
+                    b31.setUnit("021");
+                    b31.setSrNo(account);
+                    b31.setLocateRate(100.00);
+                    b31.setBranchNo(b21.getBranchNo());
+                    b31.setCurrencyCode(b21.getCurrencyCode());
+                    b31.setCustomerId(b21.getCustomerId());
+                    
+                    processCount++;
+                    if (processCount % writeSampleFrequency == 1) {
+                        b31.setSample(true);
+                    }
+                    
+                    results.add(b31);
                 }
-                
-                return b31;
+                return results;
             } else {
                 return null;
             }
         }
         
-        A22 a22 = a22Dao.findByCustomerIdAndSrNo(item.getGRB(), account, "A22");
-        if (a22 != null) {
+        List<A22> a22s = a22Dao.findByCustomerId(item.getGRB(), "A22");
+        if (a22s != null && !a22s.isEmpty()) {
             if (type == 1) {
-                A31 a31 = new A31();
-                a31.setUnit("021");
-                a31.setSrNo(account);
-                a31.setLocateRate(100.00);
-                a31.setBranchNo(a22.getBranchNo());
-                a31.setCurrencyCode(a22.getCurrencyCode());
-                a31.setCustomerId(a22.getCustomerId());
-                
-                processCount++;
-                if (processCount % writeSampleFrequency == 1) {
-                    a31.setSample(true);
+                for (A22 a22 : a22s) {
+                    A31 a31 = new A31();
+                    a31.setUnit("021");
+                    a31.setSrNo(account);
+                    a31.setLocateRate(100.00);
+                    a31.setBranchNo(a22.getBranchNo());
+                    a31.setCurrencyCode(a22.getCurrencyCode());
+                    a31.setCustomerId(a22.getCustomerId());
+                    
+                    processCount++;
+                    if (processCount % writeSampleFrequency == 1) {
+                        a31.setSample(true);
+                    }
+                    
+                    results.add(a31);
                 }
-                
-                return a31;
+                return results;
             } else {
                 return null;
             }
         }
         
-        A22 b22 = a22Dao.findByCustomerIdAndSrNo(item.getGRB(), account, "B22");
-        if (b22 != null) {
+        List<A22> b22s = a22Dao.findByCustomerId(item.getGRB(), "B22");
+        if (b22s != null && !b22s.isEmpty()) {
             if (type == 2) {
-                A31 b31 = new A31();
-                b31.setUnit("021");
-                b31.setSrNo(account);
-                b31.setLocateRate(100.00);
-                b31.setBranchNo(b22.getBranchNo());
-                b31.setCurrencyCode(b22.getCurrencyCode());
-                b31.setCustomerId(b22.getCustomerId());
-                
-                processCount++;
-                if (processCount % writeSampleFrequency == 1) {
-                    b31.setSample(true);
+                for (A22 b22 : b22s) {
+                    A31 b31 = new A31();
+                    b31.setUnit("021");
+                    b31.setSrNo(account);
+                    b31.setLocateRate(100.00);
+                    b31.setBranchNo(b22.getBranchNo());
+                    b31.setCurrencyCode(b22.getCurrencyCode());
+                    b31.setCustomerId(b22.getCustomerId());
+                    
+                    processCount++;
+                    if (processCount % writeSampleFrequency == 1) {
+                        b31.setSample(true);
+                    }
+                    
+                    results.add(b31);
                 }
-                
-                return b31;
+                return results;
             } else {
                 return null;
             }
         }
         
-        A23 a23 = a23Dao.findByCustomerIdAndSrNo(item.getGRB(), account);
-        if (a23 != null) {
+        List<A23> a23s = a23Dao.findByCustomerId(item.getGRB());
+        if (a23s != null && !a23s.isEmpty()) {
             if (type == 1) {
-                A31 a31 = new A31();
-                a31.setUnit("021");
-                a31.setSrNo(account);
-                a31.setLocateRate(100.00);
-                a31.setBranchNo(a23.getBranchNo());
-                a31.setCurrencyCode(a23.getCurrencyCode());
-                a31.setCustomerId(a23.getCustomerId());
-                
-                processCount++;
-                if (processCount % writeSampleFrequency == 1) {
-                    a31.setSample(true);
+                for (A23 a23 : a23s) {
+                    A31 a31 = new A31();
+                    a31.setUnit("021");
+                    a31.setSrNo(account);
+                    a31.setLocateRate(100.00);
+                    a31.setBranchNo(a23.getBranchNo());
+                    a31.setCurrencyCode(a23.getCurrencyCode());
+                    a31.setCustomerId(a23.getCustomerId());
+                    
+                    processCount++;
+                    if (processCount % writeSampleFrequency == 1) {
+                        a31.setSample(true);
+                    }
+                    
+                    results.add(a31);
                 }
-                
-                return a31;
+                return results;
             } else {
                 return null;
             }
--- server/batch/src/main/java/tw/com/citi/cdic/batch/item/file/A31FileWriter.java
+++ server/batch/src/main/java/tw/com/citi/cdic/batch/item/file/A31FileWriter.java
@@ -1,6 +1,5 @@
 package tw.com.citi.cdic.batch.item.file;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.batch.item.ItemWriter;
@@ -10,22 +9,16 @@ import tw.com.citi.cdic.batch.model.A31;
 
 /**
  * @author Chih-Liang Chang
- * @since 2010/11/26
+ * @since 2011/2/21
  */
-public class A31SampleFileWriter implements ItemWriter<List<A31>> {
+public class A31FileWriter implements ItemWriter<List<A31>> {
 
     private FlatFileItemWriter<A31> delegator;
 
     @Override
     public void write(List<? extends List<A31>> items) throws Exception {
         for (List<A31> itemList : items) {
-            List<A31> samples = new ArrayList<A31>();
-            for (A31 item : itemList) {
-                if (item.isSample()) {
-                    samples.add(item);
-                }
-            }
-            delegator.write(samples);
+            delegator.write(itemList);
         }
     }
 
