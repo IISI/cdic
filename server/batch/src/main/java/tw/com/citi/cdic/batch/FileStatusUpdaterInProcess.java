8d83d654004716de3e430896cb5930ddc50e22ca
LancelotLiu <lancelot.yl.liu@gmail.com>
2011-03-21 11:35:49 +0800

    加入 FileStatusUpdaterInProcess 處理每個檔案"非最後一個 step"的狀態更新，成功不處理，失敗更新為失敗。原本的 FileStatusUpdater 用於最後一個 step 及 job 結束時，成功更新狀態為已完成，失敗更新狀態為執行失敗。

---
+++ server/batch/src/main/java/tw/com/citi/cdic/batch/FileStatusUpdaterInProcess.java
@@ -0,0 +1,50 @@
+package tw.com.citi.cdic.batch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.annotation.AfterStep;
+
+import tw.com.citi.cdic.batch.dao.CDICFileStatusDao;
+import tw.com.citi.cdic.batch.model.CDICFileStatus;
+
+/**
+ * 處理非最後一個 step 的狀態更新
+ * 
+ * @author Yunglin Liu
+ * @since 2011/03/21
+ */
+public class FileStatusUpdaterInProcess {
+
+    protected static final Logger logger = LoggerFactory.getLogger(FileStatusUpdaterInProcess.class);
+
+    private CDICFileStatusDao CDICFileStatusDao;
+
+    @AfterStep
+    public void updateStatus(StepExecution stepExecution) {
+        String stepName = stepExecution.getStepName();
+        stepName = stepName.toUpperCase().substring(0, 3);
+        if (stepName.startsWith("F") || stepName.startsWith("T")) {
+            ExitStatus status = stepExecution.getExitStatus();
+            if (status == null) {
+                logger.debug("record step status, step name = {}, status = {}", stepName, stepExecution.getExitStatus()
+                        .getExitCode());
+            } else {
+                logger.debug("record step status, step name = {}, status = {}", stepName, stepExecution.getExitStatus()
+                        .and(status).getExitCode());
+            }
+            if (ExitStatus.COMPLETED.compareTo(status) != 0) {
+                CDICFileStatus fileStatus = this.CDICFileStatusDao.findByFileNo(stepName);
+                fileStatus.setStatus("5");
+                this.CDICFileStatusDao.update(fileStatus);
+                logger.debug("update step status, step name = {}, status = {}", stepName, status.getExitCode());
+            }
+        }
+    }
+
+    public void setCDICFileStatusDao(CDICFileStatusDao cDICFileStatusDao) {
+        CDICFileStatusDao = cDICFileStatusDao;
+    }
+
+}
