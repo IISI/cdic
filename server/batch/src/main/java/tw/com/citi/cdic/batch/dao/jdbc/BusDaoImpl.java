bed71a59e770c5f10823c4668b768113242d2f7b
LancelotLiu <lancelot.yl.liu@gmail.com>
2011-11-16 13:16:43 +0800

    從 Guarantor 串 BUS 時，若串不到資料，則略過此筆資料，並紀錄 log。

--- server/batch/src/main/java/tw/com/citi/cdic/batch/dao/jdbc/BusDaoImpl.java
+++ server/batch/src/main/java/tw/com/citi/cdic/batch/dao/jdbc/BusDaoImpl.java
@@ -2,6 +2,8 @@ package tw.com.citi.cdic.batch.dao.jdbc;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.simple.SimpleJdbcDaoSupport;
 
@@ -15,10 +17,20 @@ import tw.com.citi.cdic.batch.model.Bus;
  */
 public class BusDaoImpl extends SimpleJdbcDaoSupport implements BusDao {
 
+    protected final Logger logger = LoggerFactory.getLogger(BusDaoImpl.class);
+
     @Override
     public Bus findByCustNumb(String customerNo) {
-        List<Bus> buses = getSimpleJdbcTemplate().query("SELECT * FROM BUS WHERE CUST_NUMB = ?", new BusMapper(), customerNo);
-        return DataAccessUtils.requiredUniqueResult(buses);
+        List<Bus> buses = getSimpleJdbcTemplate().query("SELECT * FROM BUS WHERE CUST_NUMB = ?", new BusMapper(),
+                customerNo);
+        Bus bus = null;
+        try {
+            bus = DataAccessUtils.requiredUniqueResult(buses);
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.info("Can't find BUS. [CUST_NUMB = {}]", new Object[] { customerNo });
+        }
+        return bus;
     }
 
 }
--- server/batch/src/main/java/tw/com/citi/cdic/batch/item/SBF15AProcessor.java
+++ server/batch/src/main/java/tw/com/citi/cdic/batch/item/SBF15AProcessor.java
@@ -3,12 +3,15 @@ package tw.com.citi.cdic.batch.item;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
 
 import tw.com.citi.cdic.batch.dao.BusDao;
 import tw.com.citi.cdic.batch.dao.GuarantorDao;
 import tw.com.citi.cdic.batch.model.A41;
 import tw.com.citi.cdic.batch.model.A44;
+import tw.com.citi.cdic.batch.model.Bus;
 import tw.com.citi.cdic.batch.model.Guarantor;
 
 /**
@@ -16,6 +19,8 @@ import tw.com.citi.cdic.batch.model.Guarantor;
  * @since 2011/9/29
  */
 public class SBF15AProcessor implements ItemProcessor<A41, List<A44>> {
+
+    protected final Logger logger = LoggerFactory.getLogger(SBF15AProcessor.class);
 
     private GuarantorDao guarantorDao;
 
@@ -25,7 +30,8 @@ public class SBF15AProcessor implements ItemProcessor<A41, List<A44>> {
     public List<A44> process(A41 item) throws Exception {
         List<A44> a44List = new ArrayList<A44>();
         List<Guarantor> guarantors = guarantorDao.findByCustomerNo(item.getCustId());
-        if (guarantors == null || guarantors.isEmpty()) return null;
+        if (guarantors == null || guarantors.isEmpty())
+            return null;
         for (Guarantor guarantor : guarantors) {
             List<A44> temp = createA44s(item, guarantor);
             a44List.addAll(temp);
@@ -35,10 +41,11 @@ public class SBF15AProcessor implements ItemProcessor<A41, List<A44>> {
 
     private List<A44> createA44s(A41 item, Guarantor guarantor) {
         List<A44> a44List = new ArrayList<A44>();
-        if (guarantor.getUdf9() != null) {
+        Bus bus = null;
+        if (guarantor.getUdf9() != null && (bus = busDao.findByCustNumb(guarantor.getUdf9())) != null) {
             A44 a44 = createA44(item);
             a44.setArantId(guarantor.getUdf9());
-            a44.setArantName(busDao.findByCustNumb(guarantor.getUdf9()).getCustTitlLine1());
+            a44.setArantName(bus.getCustTitlLine1());
             a44.setCharCode(guarantor.getUdf6());
             a44.setRelCode(guarantor.getUdf7());
             a44.setCurCode(guarantor.getUdf10());
@@ -46,10 +53,10 @@ public class SBF15AProcessor implements ItemProcessor<A41, List<A44>> {
             a44.setEffDate(guarantor.getUdf8());
             a44List.add(a44);
         }
-        if (guarantor.getUdf15() != null) {
+        if (guarantor.getUdf15() != null && (bus = busDao.findByCustNumb(guarantor.getUdf15())) != null) {
             A44 a44 = createA44(item);
             a44.setArantId(guarantor.getUdf15());
-            a44.setArantName(busDao.findByCustNumb(guarantor.getUdf15()).getCustTitlLine1());
+            a44.setArantName(bus.getCustTitlLine1());
             a44.setCharCode(guarantor.getUdf13());
             a44.setRelCode(guarantor.getUdf14());
             a44.setCurCode(guarantor.getUdf17());
@@ -57,10 +64,10 @@ public class SBF15AProcessor implements ItemProcessor<A41, List<A44>> {
             a44.setEffDate(guarantor.getUdf16());
             a44List.add(a44);
         }
-        if (guarantor.getUdf22() != null) {
+        if (guarantor.getUdf22() != null && (bus = busDao.findByCustNumb(guarantor.getUdf22())) != null) {
             A44 a44 = createA44(item);
             a44.setArantId(guarantor.getUdf22());
-            a44.setArantName(busDao.findByCustNumb(guarantor.getUdf22()).getCustTitlLine1());
+            a44.setArantName(bus.getCustTitlLine1());
             a44.setCharCode(guarantor.getUdf20());
             a44.setRelCode(guarantor.getUdf21());
             a44.setCurCode(guarantor.getUdf24());
@@ -68,10 +75,10 @@ public class SBF15AProcessor implements ItemProcessor<A41, List<A44>> {
             a44.setEffDate(guarantor.getUdf23());
             a44List.add(a44);
         }
-        if (guarantor.getUdf29() != null) {
+        if (guarantor.getUdf29() != null && (bus = busDao.findByCustNumb(guarantor.getUdf29())) != null) {
             A44 a44 = createA44(item);
             a44.setArantId(guarantor.getUdf29());
-            a44.setArantName(busDao.findByCustNumb(guarantor.getUdf29()).getCustTitlLine1());
+            a44.setArantName(bus.getCustTitlLine1());
             a44.setCharCode(guarantor.getUdf27());
             a44.setRelCode(guarantor.getUdf28());
             a44.setCurCode(guarantor.getUdf31());
@@ -79,10 +86,10 @@ public class SBF15AProcessor implements ItemProcessor<A41, List<A44>> {
             a44.setEffDate(guarantor.getUdf30());
             a44List.add(a44);
         }
-        if (guarantor.getUdf36() != null) {
+        if (guarantor.getUdf36() != null && (bus = busDao.findByCustNumb(guarantor.getUdf36())) != null) {
             A44 a44 = createA44(item);
             a44.setArantId(guarantor.getUdf36());
-            a44.setArantName(busDao.findByCustNumb(guarantor.getUdf36()).getCustTitlLine1());
+            a44.setArantName(bus.getCustTitlLine1());
             a44.setCharCode(guarantor.getUdf34());
             a44.setRelCode(guarantor.getUdf35());
             a44.setCurCode(guarantor.getUdf38());
@@ -90,10 +97,10 @@ public class SBF15AProcessor implements ItemProcessor<A41, List<A44>> {
             a44.setEffDate(guarantor.getUdf37());
             a44List.add(a44);
         }
-        if (guarantor.getUdf43() != null) {
+        if (guarantor.getUdf43() != null && (bus = busDao.findByCustNumb(guarantor.getUdf43())) != null) {
             A44 a44 = createA44(item);
             a44.setArantId(guarantor.getUdf43());
-            a44.setArantName(busDao.findByCustNumb(guarantor.getUdf43()).getCustTitlLine1());
+            a44.setArantName(bus.getCustTitlLine1());
             a44.setCharCode(guarantor.getUdf41());
             a44.setRelCode(guarantor.getUdf42());
             a44.setCurCode(guarantor.getUdf45());
@@ -101,10 +108,10 @@ public class SBF15AProcessor implements ItemProcessor<A41, List<A44>> {
             a44.setEffDate(guarantor.getUdf44());
             a44List.add(a44);
         }
-        if (guarantor.getUdf51() != null) {
+        if (guarantor.getUdf51() != null && (bus = busDao.findByCustNumb(guarantor.getUdf51())) != null) {
             A44 a44 = createA44(item);
             a44.setArantId(guarantor.getUdf51());
-            a44.setArantName(busDao.findByCustNumb(guarantor.getUdf51()).getCustTitlLine1());
+            a44.setArantName(bus.getCustTitlLine1());
             a44.setCharCode(guarantor.getUdf48());
             a44.setRelCode(guarantor.getUdf49());
             a44.setCurCode(guarantor.getUdf53());
@@ -112,10 +119,10 @@ public class SBF15AProcessor implements ItemProcessor<A41, List<A44>> {
             a44.setEffDate(guarantor.getUdf52());
             a44List.add(a44);
         }
-        if (guarantor.getUdf58() != null) {
+        if (guarantor.getUdf58() != null && (bus = busDao.findByCustNumb(guarantor.getUdf58())) != null) {
             A44 a44 = createA44(item);
             a44.setArantId(guarantor.getUdf58());
-            a44.setArantName(busDao.findByCustNumb(guarantor.getUdf58()).getCustTitlLine1());
+            a44.setArantName(bus.getCustTitlLine1());
             a44.setCharCode(guarantor.getUdf56());
             a44.setRelCode(guarantor.getUdf57());
             a44.setCurCode(guarantor.getUdf60());
@@ -123,10 +130,10 @@ public class SBF15AProcessor implements ItemProcessor<A41, List<A44>> {
             a44.setEffDate(guarantor.getUdf59());
             a44List.add(a44);
         }
-        if (guarantor.getUdf65() != null) {
+        if (guarantor.getUdf65() != null && (bus = busDao.findByCustNumb(guarantor.getUdf65())) != null) {
             A44 a44 = createA44(item);
             a44.setArantId(guarantor.getUdf65());
-            a44.setArantName(busDao.findByCustNumb(guarantor.getUdf65()).getCustTitlLine1());
+            a44.setArantName(bus.getCustTitlLine1());
             a44.setCharCode(guarantor.getUdf63());
             a44.setRelCode(guarantor.getUdf64());
             a44.setCurCode(guarantor.getUdf67());
@@ -134,10 +141,10 @@ public class SBF15AProcessor implements ItemProcessor<A41, List<A44>> {
             a44.setEffDate(guarantor.getUdf66());
             a44List.add(a44);
         }
-        if (guarantor.getUdf72() != null) {
+        if (guarantor.getUdf72() != null && (bus = busDao.findByCustNumb(guarantor.getUdf72())) != null) {
             A44 a44 = createA44(item);
             a44.setArantId(guarantor.getUdf72());
-            a44.setArantName(busDao.findByCustNumb(guarantor.getUdf72()).getCustTitlLine1());
+            a44.setArantName(bus.getCustTitlLine1());
             a44.setCharCode(guarantor.getUdf70());
             a44.setRelCode(guarantor.getUdf71());
             a44.setCurCode(guarantor.getUdf74());
@@ -145,10 +152,10 @@ public class SBF15AProcessor implements ItemProcessor<A41, List<A44>> {
             a44.setEffDate(guarantor.getUdf73());
             a44List.add(a44);
         }
-        if (guarantor.getUdf79() != null) {
+        if (guarantor.getUdf79() != null && (bus = busDao.findByCustNumb(guarantor.getUdf79())) != null) {
             A44 a44 = createA44(item);
             a44.setArantId(guarantor.getUdf79());
-            a44.setArantName(busDao.findByCustNumb(guarantor.getUdf79()).getCustTitlLine1());
+            a44.setArantName(bus.getCustTitlLine1());
             a44.setCharCode(guarantor.getUdf77());
             a44.setRelCode(guarantor.getUdf78());
             a44.setCurCode(guarantor.getUdf81());
@@ -156,10 +163,10 @@ public class SBF15AProcessor implements ItemProcessor<A41, List<A44>> {
             a44.setEffDate(guarantor.getUdf80());
             a44List.add(a44);
         }
-        if (guarantor.getUdf86() != null) {
+        if (guarantor.getUdf86() != null && (bus = busDao.findByCustNumb(guarantor.getUdf86())) != null) {
             A44 a44 = createA44(item);
             a44.setArantId(guarantor.getUdf86());
-            a44.setArantName(busDao.findByCustNumb(guarantor.getUdf86()).getCustTitlLine1());
+            a44.setArantName(bus.getCustTitlLine1());
             a44.setCharCode(guarantor.getUdf84());
             a44.setRelCode(guarantor.getUdf85());
             a44.setCurCode(guarantor.getUdf88());
@@ -167,10 +174,10 @@ public class SBF15AProcessor implements ItemProcessor<A41, List<A44>> {
             a44.setEffDate(guarantor.getUdf87());
             a44List.add(a44);
         }
-        if (guarantor.getUdf93() != null) {
+        if (guarantor.getUdf93() != null && (bus = busDao.findByCustNumb(guarantor.getUdf93())) != null) {
             A44 a44 = createA44(item);
             a44.setArantId(guarantor.getUdf93());
-            a44.setArantName(busDao.findByCustNumb(guarantor.getUdf93()).getCustTitlLine1());
+            a44.setArantName(bus.getCustTitlLine1());
             a44.setCharCode(guarantor.getUdf91());
             a44.setRelCode(guarantor.getUdf92());
             a44.setCurCode(guarantor.getUdf95());
@@ -178,10 +185,10 @@ public class SBF15AProcessor implements ItemProcessor<A41, List<A44>> {
             a44.setEffDate(guarantor.getUdf94());
             a44List.add(a44);
         }
-        if (guarantor.getUdf100() != null) {
+        if (guarantor.getUdf100() != null && (bus = busDao.findByCustNumb(guarantor.getUdf100())) != null) {
             A44 a44 = createA44(item);
             a44.setArantId(guarantor.getUdf100());
-            a44.setArantName(busDao.findByCustNumb(guarantor.getUdf100()).getCustTitlLine1());
+            a44.setArantName(bus.getCustTitlLine1());
             a44.setCharCode(guarantor.getUdf98());
             a44.setRelCode(guarantor.getUdf99());
             a44.setCurCode(guarantor.getUdf102());
@@ -189,10 +196,10 @@ public class SBF15AProcessor implements ItemProcessor<A41, List<A44>> {
             a44.setEffDate(guarantor.getUdf101());
             a44List.add(a44);
         }
-        if (guarantor.getUdf107() != null) {
+        if (guarantor.getUdf107() != null && (bus = busDao.findByCustNumb(guarantor.getUdf107())) != null) {
             A44 a44 = createA44(item);
             a44.setArantId(guarantor.getUdf107());
-            a44.setArantName(busDao.findByCustNumb(guarantor.getUdf107()).getCustTitlLine1());
+            a44.setArantName(bus.getCustTitlLine1());
             a44.setCharCode(guarantor.getUdf105());
             a44.setRelCode(guarantor.getUdf106());
             a44.setCurCode(guarantor.getUdf109());
