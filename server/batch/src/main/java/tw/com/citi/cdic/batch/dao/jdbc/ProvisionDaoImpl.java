4f18d671a00f67841073ff3cc344179b98745867
LancelotLiu <lancelot.yl.liu@gmail.com>
2011-11-16 13:21:45 +0800

    從 F12A 串 Provision 若串不到資料，則不進行 EvlRank 的設定。

--- server/batch/src/main/java/tw/com/citi/cdic/batch/dao/jdbc/ProvisionDaoImpl.java
+++ server/batch/src/main/java/tw/com/citi/cdic/batch/dao/jdbc/ProvisionDaoImpl.java
@@ -2,6 +2,8 @@ package tw.com.citi.cdic.batch.dao.jdbc;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.simple.SimpleJdbcDaoSupport;
 
@@ -13,13 +15,22 @@ import tw.com.citi.cdic.batch.model.Provision;
  * @author Chih-Liang Chang
  * @since 2011/9/26
  */
-public class ProvisionDaoImpl extends SimpleJdbcDaoSupport implements
-        ProvisionDao {
+public class ProvisionDaoImpl extends SimpleJdbcDaoSupport implements ProvisionDao {
+
+    protected final Logger logger = LoggerFactory.getLogger(ProvisionDaoImpl.class);
 
     @Override
     public Provision findByReferenceNo(String referenceNo) {
-        List<Provision> provisions = super.getSimpleJdbcTemplate().query("SELECT * FROM PROVISION WHERE CONTRACT_REFERENCE_NO = ?", new ProvisionMapper(), referenceNo);
-        return DataAccessUtils.requiredUniqueResult(provisions);
+        List<Provision> provisions = super.getSimpleJdbcTemplate().query(
+                "SELECT * FROM PROVISION WHERE CONTRACT_REFERENCE_NO = ?", new ProvisionMapper(), referenceNo);
+        Provision provision = null;
+        try {
+            provision = DataAccessUtils.requiredUniqueResult(provisions);
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.info("Can't find provision. [CONTRACT_REFERENCE_NO = {}]", new Object[] { referenceNo });
+        }
+        return provision;
     }
 
 }
--- server/batch/src/main/java/tw/com/citi/cdic/batch/item/SBF12AProcessor.java
+++ server/batch/src/main/java/tw/com/citi/cdic/batch/item/SBF12AProcessor.java
@@ -21,30 +21,34 @@ public class SBF12AProcessor implements ItemProcessor<A41, A41> {
     @Override
     public A41 process(A41 item) throws Exception {
         logger.debug("A41 SRNO = {}", item.getSrNo());
-        Provision provision = provisionDao.findByReferenceNo(item.getSrNo().substring(0, 14));
         if ("WTDT".equals(item.getCharCode())) {
             item.setEvlRank("5");
         } else if (item.getSrNo().length() < 16) {
             return item;
-        } else if ("-1".equals(item.getSrNo().substring(14, 16))) {
-            if (provision.getProvision5Secured() != null) {
-                item.setEvlRank("5");
-            } else if (provision.getProvision4Secured() != null) {
-                item.setEvlRank("4");
-            } else if (provision.getProvision32Secured() != null || provision.getProvision31Secured() != null) {
-                item.setEvlRank("3");
-            } else if (provision.getProvision2Secured() != null) {
-                item.setEvlRank("2");
-            }
-        } else if ("-2".equals(item.getSrNo().substring(14, 16))) {
-            if (provision.getProvision5Clean() != null) {
-                item.setEvlRank("5");
-            } else if (provision.getProvision4Clean() != null) {
-                item.setEvlRank("4");
-            } else if (provision.getProvision32Clean() != null || provision.getProvision31Clean() != null) {
-                item.setEvlRank("3");
-            } else if (provision.getProvision2Clean() != null) {
-                item.setEvlRank("2");
+        } else {
+            Provision provision = provisionDao.findByReferenceNo(item.getSrNo().substring(0, 14));
+            if (provision != null) {
+                if ("-1".equals(item.getSrNo().substring(14, 16))) {
+                    if (provision.getProvision5Secured() != null) {
+                        item.setEvlRank("5");
+                    } else if (provision.getProvision4Secured() != null) {
+                        item.setEvlRank("4");
+                    } else if (provision.getProvision32Secured() != null || provision.getProvision31Secured() != null) {
+                        item.setEvlRank("3");
+                    } else if (provision.getProvision2Secured() != null) {
+                        item.setEvlRank("2");
+                    }
+                } else if ("-2".equals(item.getSrNo().substring(14, 16))) {
+                    if (provision.getProvision5Clean() != null) {
+                        item.setEvlRank("5");
+                    } else if (provision.getProvision4Clean() != null) {
+                        item.setEvlRank("4");
+                    } else if (provision.getProvision32Clean() != null || provision.getProvision31Clean() != null) {
+                        item.setEvlRank("3");
+                    } else if (provision.getProvision2Clean() != null) {
+                        item.setEvlRank("2");
+                    }
+                }
             }
         }
         return item;
