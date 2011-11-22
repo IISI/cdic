13f613e29f8d73694b887e23ac405a8843ba8f52
LancelotLiu <lancelot.yl.liu@gmail.com>
2011-11-22 13:12:56 +0800

    修正 A44SampleFileWriter，因為 f15AProcessor 回傳的資料是 List<List<A44>>，但 f15SampleProcessor 回傳的資料是 List<A44>，故修正之使其能同時處理兩種型別的傳入資料。

--- server/batch/src/main/java/tw/com/citi/cdic/batch/item/file/A44SampleFileWriter.java
+++ server/batch/src/main/java/tw/com/citi/cdic/batch/item/file/A44SampleFileWriter.java
@@ -15,15 +15,26 @@ import tw.com.citi.cdic.batch.utils.MaskUtils;
 public class A44SampleFileWriter extends FlatFileItemWriter<A44> {
 
     @Override
-    public void write(List<? extends A44> items) throws Exception {
+    public void write(List items) throws Exception {
         List<A44> a44s = new ArrayList<A44>();
-        for (A44 a44 : items) {
-            a44.setCustId(MaskUtils.mask(a44.getCustId(), 6));
-            a44.setArantId(MaskUtils.mask(a44.getArantId(), 6));
-            a44.setArantName(MaskUtils.mask(a44.getArantName(), 2));
-            a44s.add(a44);
+        for (Object item : items) {
+            if (item instanceof A44) {
+                A44 a44 = (A44) item;
+                a44s.add(mask(a44));
+            } else if (item instanceof List) {
+                for (A44 a44 : (List<A44>) item) {
+                    a44s.add(mask(a44));
+                }
+            }
         }
         super.write(a44s);
+    }
+
+    private A44 mask(A44 a44) {
+        a44.setCustId(MaskUtils.mask(a44.getCustId(), 6));
+        a44.setArantId(MaskUtils.mask(a44.getArantId(), 6));
+        a44.setArantName(MaskUtils.mask(a44.getArantName(), 2));
+        return a44;
     }
 
 }
